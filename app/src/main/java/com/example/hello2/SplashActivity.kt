4b126268aea4c07155d7a58e4df0f46eff7bc774
ngaukonojane <namtendaisabella@gmail.com>
2020-08-22 01:39:24 +0300

    first commit

--- app/src/main/java/com/example/hello2/SplashActivity.kt
+++ app/src/main/java/com/example/hello2/SplashActivity.kt
@@ -7,6 +7,20 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+
+
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        var accessToken = sharedPreferences.getString("ACCESS_TOKEN_KEY", "")
+
+        if (accessToken.isNullOrEmpty()) {
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(baseContext, CoursesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
-
--- app/src/main/res/layout/loginResponse.kt
+++ app/src/main/res/layout/loginResponse.kt
@@ -1,4 +1,8 @@
 package layout
 
 class loginResponse {
+    data class LoginResponse(
+        @SerializedName("access_token") var accessToken: String,
+        @SerializedName("message") var message: String
+    )
 }
