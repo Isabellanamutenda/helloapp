90622d6b7f21e57e397461ec7379871b1eb15ef3
ngaukonojane <namtendaisabella@gmail.com>
2020-09-14 08:12:54 +0300

    first commit

--- app/src/main/java/com/example/hello2/models/Course_Adapter.kt
+++ app/src/main/java/com/example/hello2/models/Course_Adapter.kt
@@ -1,5 +1,8 @@
 package com.example.hello2.models
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hello2.Course
 import com.example.hello2.R
@@ -8,7 +11,7 @@ class Course_Adapter {
     class CoursesAdapter(var courseList: List<Course>) :
         RecyclerView.Adapter<CoursesAdapter.CoursesViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoursesViewHolder {
+        fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoursesViewHolder {
             var itemView =
                 LayoutInflater.from(parent.context).inflate(R.layout.row_course_item, parent, false)
             return CoursesViewHolder(
@@ -16,11 +19,11 @@ class Course_Adapter {
             )
         }
 
-        override fun getItemCount(): Int {
+        fun getItemCount(): Int {
             return courseList.size
         }
 
-        override fun onBindViewHolder(holder: CoursesViewHolder, position: Int) {
+        fun onBindViewHolder(holder: CoursesViewHolder, position: Int) {
             holder.rowView.tvCourseCode.text = courseList[position].courseCode
             holder.rowView.tvCourseName.text = courseList[position].courseName
             holder.rowView.tvDescription.text = courseList[position].description
@@ -31,3 +34,4 @@ class Course_Adapter {
     }
 
 }
+class CoursesViewHolder(val rowView: View) : RecyclerView.ViewHolder(rowView)
